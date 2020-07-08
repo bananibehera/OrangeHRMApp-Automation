aaccb656084d139fc2167398727aef78c16eb706
bananibehera <57158122+bananibehera@users.noreply.github.com>
2020-07-08 17:46:05 +1200

    implemented specflow

---
+++ FeatureFile/ManageAdminUser.feature
@@ -0,0 +1,21 @@
+﻿Feature: ManageAdminUser
+	As Orange HRMs administration, 
+	I would like to manage user profiles within the portal
+
+@mytag
+Scenario: Add a new admin user
+	Given I login to Orange HRM portal as an administration
+	And I navigate to systems user page
+	Then I should able to create a new user successfully
+
+Scenario: Edit an existing admin user
+	Given I login to Orange HRM portal as an administration
+	And I navigate to systems user page
+	Then I should able to edit an existing user successfully
+
+Scenario: Delete an existing admin user profile
+    Given I login to Orange HRM portal as an administration
+	And I navigate to systems user page
+	Then I should able to delete an existing user successfully
+
+
---
+++ StepDefinition/ManageAdminUserSteps.cs
@@ -0,0 +1,49 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using TechTalk.SpecFlow;
+
+namespace OrangeHRMJune2020.StepDefinition
+{
+    [Binding]
+    public class ManageAdminUserSteps
+    {
+        IWebDriver driver = new ChromeDriver();
+        [Given(@"I login to Orange HRM portal as an administration")]
+        public void GivenILoginToOrangeHRMPortalAsAnAdministration()
+        {
+            var loginPage = new LoginPage(driver);
+            loginPage.LoginSuccess();
+        }
+        
+        [Given(@"I navigate to systems user page")]
+        public void GivenINavigateToSystemsUserPage()
+        {
+            var dashboardPage = new DashBoardPage(driver);
+            dashboardPage.ClickUsers();
+        }
+        
+        [Then(@"I should able to create a new user successfully")]
+        public void ThenIShouldAbleToCreateANewUserSuccessfully()
+        {
+            var systemUsersPage = new SystemUsersPage(driver);
+            systemUsersPage.ClickAdd();
+            systemUsersPage.AddAndSaveUser();
+            systemUsersPage.VerifyUser();
+        }
+
+        [Then(@"I should able to edit an existing user successfully")]
+        public void ThenIShouldAbleToEditAnExistingUserSuccessfully()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"I should able to delete an existing user successfully")]
+        public void ThenIShouldAbleToDeleteAnExistingUserSuccessfully()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+
+    }
+}
